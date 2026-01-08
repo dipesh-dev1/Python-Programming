446312366e12d26abe66bb4dd28fc7dec18a2540
Dipesh Chaudhary <chaudharydipesh190@gmail.com>
2026-01-08 11:16:46 +0545

    Used isinstance() function

---
+++ data_types.py
@@ -0,0 +1,41 @@
+#Here are the most common data types used in Python:
+
+#Integer: A whole number without decimals, for example, 10 or -5.
+my_integer_var = 10
+print('Integer:', my_integer_var)
+
+#Float: A number with a decimal point, like 4.41 or -0.4.
+my_float_var = 4.66
+print('Float:', my_float_var)
+
+#String: A sequence of characters enclosed in single or double quotation marks like 'Hello world!'.
+my_string_var = 'Dipesh'
+print('String:', my_string_var)
+
+#Boolean: A true or false type, written as True or False.
+my_boolean_var = True
+print('Boolean:', my_boolean_var)
+
+#Set: An unordered collection of unique elements, like {4, 2, 0}.
+my_set_var = {3, 6, 1}
+print('Set:', my_set_var)
+
+#Dictionary: A collection of key-value pairs enclosed in curly braces, like {'name': 'Dipesh Chaudhary', 'age': 20}.
+my_dictionary_var = {'name': 'Dipesh Chaudhary', 'age': 20}
+print('Dictionary:', my_dictionary_var)
+
+#Tuple: An immutable ordered collection, enclosed in brackets, like (7, 8, 4).
+my_tuple_var = (5, 2, 9)
+print('Tuple:', my_tuple_var)
+
+#Range: A sequence of numbers, often used in loops, for example, range(5).
+my_range_var = range(5)
+print('Range:', my_range_var)
+
+#List: An ordered collection of elements that supports different data types.
+my_list = ['Dipesh', 20, True, 3.67]
+print('List:', my_list)
+
+#None: A special value that represents the absence of a value.
+my_none_var = None
+print('None:', my_none_var)
